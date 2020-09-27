b288c0d81b16bcadb39e66b3158d1326133f641c
Hayley van Waas <hayleyavw@gmail.com>
2020-09-27 15:05:24 +0800

    Improved page styling.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const [income, setIncome] = useState({
-    totalIncome: "0.00",
+    grossIncome: "0.00",
     tax: "0.00",
     splurge: "0.00",
     smile: "0.00",
@@ -14,18 +14,21 @@ function App() {
 
   const handleChange = (event: any) => {
     const { value } = event.target;
+    console.log(event.target.value.length + 1);
     if (!value) {
+      event.target.style.width = "120px";
       return;
     }
+    event.target.style.width = (event.target.value.length + 1) * 16 + "px";
     setIncome((prevIncome) => ({
       ...prevIncome,
-      totalIncome: value,
+      grossIncome: value,
     }));
   };
 
   useEffect(() => {
-    const tax = parseInt(income.totalIncome) * 0.25;
-    const netIncome = parseInt(income.totalIncome) - tax;
+    const tax = parseInt(income.grossIncome) * 0.25;
+    const netIncome = parseInt(income.grossIncome) - tax;
     const splurge = (netIncome * 0.1).toFixed(2);
     const smile = (netIncome * 0.1).toFixed(2);
     const mojo = (netIncome * 0.2).toFixed(2);
@@ -38,62 +41,68 @@ function App() {
       mojo: mojo,
       everyday: everyday,
     }));
-  }, [income.totalIncome]);
+  }, [income.grossIncome]);
 
   return (
     <div className="app">
       <h1>Income Splitter</h1>
-      <input
-        type="number"
-        step="0.01"
-        placeholder="1000.00"
-        onChange={(event) => handleChange(event)}
-        // onChange={(event) => setIncome(event.target.value)}
-      ></input>
-      <div className="account" id="splurge">
-        <h3>Tax</h3>
-        <div className="result">
-          <p>{income.tax}</p>
-          <button>
-            <img src={copyIcon} />
-          </button>
+      <div id="accounts">
+        <div className="account" id="gross-income">
+          <h3>Gross Income</h3>
+          <div className="result">
+            <p>$</p>
+            <input
+              type="number"
+              step="0.01"
+              placeholder="1000.00"
+              onChange={(event) => handleChange(event)}
+            ></input>
+          </div>
         </div>
-      </div>
-      <hr />
-      <div className="account" id="splurge">
-        <h3>Splurge</h3>
-        <div className="result">
-          <p>{income.splurge}</p>
-          <button>
-            <img src={copyIcon} />
-          </button>
+        <div className="account" id="tax">
+          <h3>Tax</h3>
+          <div className="result">
+            <p>${income.tax}</p>
+            <button>
+              <img src={copyIcon} alt="Copy paste icon." />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="account" id="smile">
-        <h3>Smile</h3>
-        <div className="result">
-          <p>{income.smile}</p>
-          <button>
-            <img src={copyIcon} />
-          </button>
+        <div className="account" id="splurge">
+          <h3>Splurge</h3>
+          <div className="result">
+            <p>${income.splurge}</p>
+            <button>
+              <img src={copyIcon} alt="Copy paste icon." />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="account" id="mojo">
-        <h3>Mojo</h3>
-        <div className="result">
-          <p>{income.mojo}</p>
-          <button>
-            <img src={copyIcon} />
-          </button>
+        <div className="account" id="smile">
+          <h3>Smile</h3>
+          <div className="result">
+            <p>${income.smile}</p>
+            <button>
+              <img src={copyIcon} alt="Copy paste icon." />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="account" id="everyday">
-        <h3>Everyday</h3>
-        <div className="result">
-          <p>{income.everyday}</p>
-          <button>
-            <img src={copyIcon} />
-          </button>
+        <div className="account" id="mojo">
+          <h3>Mojo</h3>
+          <div className="result">
+            <p>${income.mojo}</p>
+            <button>
+              <img src={copyIcon} alt="Copy paste icon." />
+            </button>
+          </div>
+        </div>
+        <div className="account" id="everyday">
+          <h3>Everyday</h3>
+          <div className="result">
+            <p>${income.everyday}</p>
+            <button>
+              <img src={copyIcon} alt="Copy paste icon." />
+            </button>
+          </div>
         </div>
       </div>
     </div>
